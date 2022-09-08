a3bce6eba74d4124f1dd8e0fa95fcd0cd36ff630
J3pp3s <jespher2010@live.com>
2022-09-08 16:20:38 +0200

    Styling navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import DetailPage from './sass/pages/DetailPage';
 import Homepage from './sass/pages/Homepage';
 import LoginPage from './sass/pages/LoginPage';
+import DetailPage from './sass/pages/DetailPage';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <>
+    <Navigation />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/login" component={LoginPage} />
