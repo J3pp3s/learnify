aecc39fbc908ac4868cc984ea5f35d1abd0f848e
J3pp3s <jespher2010@live.com>
2022-09-08 21:07:21 +0200

    Fixed front end in course.scss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Homepage from './sass/pages/Homepage';
-import LoginPage from './sass/pages/LoginPage';
-import DetailPage from './sass/pages/DetailPage';
+import Homepage from './pages/Homepage';
+import LoginPage from './pages/LoginPage';
+import DetailPage from './pages/DetailPage';
 import Navigation from './components/Navigation';
+import "antd/dist/antd.css";
 
 function App() {
   return (
