ccdd45872d0d3469d45fa64631ec41060a5c0bd1
J3pp3s <jespher2010@live.com>
2022-09-13 11:25:15 +0200

    Added requirement and learning to Db

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Entity
 {
-    public class Course
+    public class Course : BaseEntity
     {
-        public Guid Id { get; set; }
 
         public string Title { get; set; }
 
@@ -15,5 +16,20 @@ namespace Entity
         public decimal Rating { get; set; }
 
         public string Image { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string Decription { get; set; }
+
+        public int Students { get; set; }
+
+        public string Language { get; set; }
+
+        public string Level { get; set; }
+
+        public ICollection<Requirement> Requirments { get; set; }
+
+        public ICollection<Learning> Learnings { get; set; }
+
     }
 }
