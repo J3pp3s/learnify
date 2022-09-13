a7b3417340f6d746530ece1aaf6eff80f2aab464
J3pp3s <jespher2010@live.com>
2022-09-13 14:42:01 +0200

    Updated databse and migration

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -19,7 +19,7 @@ namespace Entity
 
         public string SubTitle { get; set; }
 
-        public string Decription { get; set; }
+        public string Description { get; set; }
 
         public int Students { get; set; }
 
@@ -30,6 +30,12 @@ namespace Entity
         public ICollection<Requirement> Requirments { get; set; }
 
         public ICollection<Learning> Learnings { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
 
     }
 }
