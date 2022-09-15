16d044c163620578b5dc0ae61bf0c96d8fc07d3e
J3pp3s <jespher2010@live.com>
2022-09-15 13:10:26 +0200

    Comments

--- Entity/Specifications/CoursesWithCategoriesSpecification.cs
+++ Entity/Specifications/CoursesWithCategoriesSpecification.cs
@@ -5,11 +5,13 @@ namespace Entity.Specifications
 {
     public class CoursesWithCategoriesSpecification : BaseSpecification<Course>
     {
+        // Empty constructor for List Methods
         public CoursesWithCategoriesSpecification()
         {
             IncludeMethod(c => c.Category);
         }
 
+        // For criteria
         public CoursesWithCategoriesSpecification(Guid id) : base(x => x.Id == id)
         {
             IncludeMethod(c => c.Requirements);
