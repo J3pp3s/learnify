4b24fd449ecfbfca09f8fa878dfe6225e33209b7
J3pp3s <jespher2010@live.com>
2022-09-21 13:43:54 +0200

    Installed redux

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+import { LoginState } from "../redux/loginReducer";
+
 const LoginPage = () => {
-    return <h1>Login Page</h1>;
+
+    const {visits} = useSelector((state: LoginState) => state)
+
+    return <h1>Number of visits: {visits}</h1>;
   };
   
   export default LoginPage;
