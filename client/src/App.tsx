35150b90eab59868ff8276190dabf4c3ead0ee05
J3pp3s <jespher2010@live.com>
2022-09-25 20:31:27 +0200

    Refactor basketslice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,31 +10,19 @@ import CategoryPage from './pages/CategoryPage';
 import DescriptionPage from './pages/DescriptionPage';
 import BasketPage from './pages/BasketPage';
 import { useEffect } from 'react';
-import agent from './actions/agent';
 import { useAppDispatch } from './redux/store/configureStore';
-import { setBasket } from './redux/slice/basketSlice';
+import { fetchBasketAsync } from './redux/slice/basketSlice';
 import Dashboard from './pages/Dashboard';
 import { getUser } from './redux/slice/userSlice';
 
 function App() {
   const dispatch = useAppDispatch();
 
-  function getCookie(name: string) {
-    return (
-      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() ||
-      ''
-    );
-  }
-
   useEffect(() => {
-    const clientId = getCookie('clientId');
-    if (clientId) {
-      agent.Baskets.get()
-        .then((basket) => dispatch(setBasket(basket)))
-        .catch((error) => console.log(error));
-    }
+    dispatch(fetchBasketAsync());
     dispatch(getUser());
   }, [dispatch]);
+
   return (
     <>
       <Navigation />
