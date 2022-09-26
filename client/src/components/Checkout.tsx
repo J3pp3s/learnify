18f7fc0c379afb5fa1f53998d813f8a19479ebe4
J3pp3s <jespher2010@live.com>
2022-09-26 10:42:49 +0200

    Clear basket endpoint

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -2,64 +2,117 @@ import {
     CardCvcElement,
     CardExpiryElement,
     CardNumberElement,
+    useElements,
+    useStripe,
   } from "@stripe/react-stripe-js";
-  import { Card, Form, Input } from "antd";
-  import { ChangeEvent, useState } from "react";
+  import { Card, Form, Input, notification } from "antd";
+  import { SyntheticEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
+import agent from "../actions/agent";
+import { removeBasket } from "../redux/slice/basketSlice";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
   import CheckoutSummary from "./CheckoutSummery";
   
   const Checkout = () => {
     const [cardName, setCardName] = useState<string>("");
+
+    const [form] = Form.useForm();
   
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const stripe = useStripe();
+    const elements = useElements();
+  
+    const dispatch = useAppDispatch();
+    const history = useHistory();
+  
+    const { basket } = useAppSelector((state) => state.basket);
+  
+    const handleChange = (e: any) => {
       setCardName(e.target.value);
     };
   
-    const [form] = Form.useForm();
+    const handlePayment = async (event: SyntheticEvent) => {
+      event.preventDefault();
+      if (!stripe || !elements) return;
+  
+      try {
+        const cardElement = elements.getElement(CardNumberElement);
+  
+        const paymentResult = await stripe.confirmCardPayment(
+          basket?.clientSecret!,
+          {
+            payment_method: {
+              card: cardElement!,
+              billing_details: {
+                name: cardName,
+              },
+            },
+          }
+        );
+        if (paymentResult.paymentIntent?.status === "succeeded") {
+          notification.success({
+            message: "Your payment is successful",
+          });
+          dispatch(removeBasket());
+          await agent.Baskets.clear();
+          setTimeout(() => {
+            history.push("/profile");
+          }, 1000);
+        } else {
+          notification.error({
+            message: paymentResult.error?.message!,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
   
     return (
-      <div className="checkout">
-        <div className="checkout__form">
-          <h1>Checkout Page</h1>
-          <Card title="Fill your Card details here">
-            <Form form={form} layout="vertical">
-              <Form.Item
-                name="cardName"
-                rules={[
-                  { required: true, message: "Card Name is required", min: 5 },
-                ]}
-                label="Name on Card"
-              >
-                <Input
+      <>
+        <div className="checkout">
+          <div className="checkout__form">
+            <h1>Checkout Page</h1>
+            <Card title="Fill your Card details here">
+              <Form name="payment" form={form} layout="vertical">
+                <Form.Item
                   name="cardName"
-                  placeholder="Mention the name on your card"
-                  value={cardName}
-                  onChange={handleChange}
-                />
-              </Form.Item>
-              <Form.Item label="Card Number">
-                <div className="stripe-input">
-                  <CardNumberElement />
+                  rules={[
+                    { required: true, message: "Card Name is required", min: 5 },
+                  ]}
+                  label="Name on Card"
+                >
+                  <Input
+                    name="cardName"
+                    onChange={handleChange}
+                    value={cardName}
+                    placeholder="Mention the name on your card"
+                  />
+                </Form.Item>
+                <Form.Item label="Card Number">
+                  <div className="stripe-input">
+                    <CardNumberElement />
+                  </div>
+                </Form.Item>
+                <div className="inline">
+                  <Form.Item label="Expiry Date">
+                    <div className="stripe-input">
+                      <CardExpiryElement />
+                    </div>
+                  </Form.Item>
+                  <Form.Item label="CVV">
+                    <div className="stripe-input">
+                      <CardCvcElement />
+                    </div>
+                  </Form.Item>
                 </div>
-              </Form.Item>
-              <div className="inline">
-                <Form.Item label="Expiry Date">
-                  <div className="stripe-input">
-                    <CardExpiryElement />
-                  </div>
-                </Form.Item>
-                <Form.Item label="CVV">
-                  <div className="stripe-input">
-                    <CardCvcElement />
-                  </div>
-                </Form.Item>
-              </div>
-            </Form>
-          </Card>
+              </Form>
+            </Card>
+          </div>
+          <div className="checkout__summary">
+            <CheckoutSummary />
+          </div>
         </div>
-        <div className="checkout__summary">
-          <CheckoutSummary />
-        </div>
-      </div>
+      </>
     );
   };
   
