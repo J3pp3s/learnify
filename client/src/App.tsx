12665a3096dec7023da4bbb43b2363d70a06bfb5
J3pp3s <jespher2010@live.com>
2022-09-26 14:11:04 +0200

    Show loading symbol

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,21 +9,34 @@ import Categories from './components/Categories';
 import CategoryPage from './pages/CategoryPage';
 import DescriptionPage from './pages/DescriptionPage';
 import BasketPage from './pages/BasketPage';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useAppDispatch } from './redux/store/configureStore';
 import { fetchBasketAsync } from './redux/slice/basketSlice';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import CheckoutPage from './pages/CheckoutPage';
 import { fetchCurrentUser } from './redux/slice/userSlice';
+import Loading from './components/Loading';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(fetchBasketAsync());
-    dispatch(fetchCurrentUser());
+  const appInit = useCallback(async () => {
+    try {
+      await dispatch(fetchBasketAsync());
+      await dispatch(fetchCurrentUser());
+  
+      } catch (err: any) {
+        console.log(err);
+      }
   }, [dispatch]);
+
+  useEffect(() => {
+    appInit().then(() => setLoading(false));
+  }, [appInit]);
+
+  if(loading) return <Loading />
 
   return (
     <>
